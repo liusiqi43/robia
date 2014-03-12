35e6a6bb123fa483405ac5971553038b344e8c87
liusiqi <me@siqi.fr>
2014-03-12 12:18:50 +0100

    vq2 updated to 1.33

--- vq2-1.23/build/CMakeFiles/packages.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/packages"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/packages.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
