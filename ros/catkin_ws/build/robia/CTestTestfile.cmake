a4e8d6e6dc646a379f39b23cf57c9816148ee0ca
pierre <pierrelouislee@yahoo.fr>
2014-04-04 13:46:44 +0200

    build/ devel/ clean up

--- ros/catkin_ws/build/robia/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/drone/Documents/robia/ros/catkin_ws/src/robia
-# Build directory: /home/drone/Documents/robia/ros/catkin_ws/build/robia
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
