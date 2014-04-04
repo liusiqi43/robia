4ec3cc76c508e89df84522a08cffce10a4560ef1
liusiqi <me@siqi.fr>
2014-04-04 16:09:52 +0200

    hover if 0.5s idle

--- ros/catkin_ws/src/robia/src/keyboard_cmd.cpp
+++ ros/catkin_ws/src/robia/src/keyboard_cmd.cpp
@@ -1,6 +1,8 @@
 #include <termios.h>
 
 #include "ros/ros.h"
+#include "ros/time.h"
+
 #include "std_msgs/Empty.h"
 
 #include "ARDrone.h"
@@ -25,13 +27,15 @@ char nonBlockingGetCh() {
 
   int main(int argc, char **argv)
   {
+    const int FREQ = 100;
     ros::init(argc, argv, "keyboard_cmd");
 
     ros::NodeHandle n;
 
-    ros::Rate loop_rate(100);
+    ros::Rate loop_rate(FREQ);
 
     ARDrone *drone = new ARDrone();
+    int idleCount = -1;
 
     while (ros::ok())
     {
@@ -40,50 +44,69 @@ char nonBlockingGetCh() {
       switch(c) {
         case 't' : {
          drone->takeOff();
+         idleCount = 0;
          break;
        }
        case 'l' : {
          drone->land();
+         idleCount = -1; 
          break;
        }
        case 'r' : {
         drone->reset();
+        idleCount = -1; 
         break;
       }
       case 'w' : {
         drone->moveForward();
+        idleCount = 0; 
         break;
       }
       case 's' : {
         drone->moveBackward();
+        idleCount = 0; 
         break;
       }
       case 'a' : {
         drone->moveLeft();
+        idleCount = 0; 
         break;
       }
       case 'd' : {
         drone->moveRight();
+        idleCount = 0; 
         break;
       }
       case 'u' : {
         drone->moveUp();
+        idleCount = 0; 
         break;
       }
       case 'j' : {
         drone->moveDown();
+        idleCount = 0; 
         break;
       }
       case 'e' : {
         drone->rotateRight();
+        idleCount = 0; 
         break;
       }
       case 'q' : {
         drone->rotateLeft();
+        idleCount = 0; 
         break;
       }
       case -1: {
-        drone->hover();
+        if (idleCount >= 0) {
+          ++idleCount;
+        }
+        // we count 1sec and we do one hover
+        // we wait for next action and count for next hover
+        if (idleCount >= FREQ/2){
+          drone->hover();
+          idleCount = -1; 
+        }
       }
     }
 
